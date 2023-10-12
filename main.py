73ace2cdcd53d59c5ea0a5087b6845e499f1d730
Roberto David Morales Ramos <davld7@outlook.com>
2023-10-11 20:31:30 -0600

    added styles

--- main.py
+++ main.py
@@ -9,3 +9,10 @@ async def index():
     path = "templates/index.html"
     media_type = "text/html"
     return FileResponse(path=path, media_type=media_type)
+
+
+@app.get("/styles.css", response_class=FileResponse, status_code=status.HTTP_200_OK)
+async def index():
+    path = "static/css/styles.css"
+    media_type = "text/css"
+    return FileResponse(path=path, media_type=media_type)
