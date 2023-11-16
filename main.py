43384ec8d65f1babb14e07c17edc4aac75223aad
Roberto David Morales Ramos <davld7@outlook.com>
2023-11-15 20:44:25 -0600

    added vehicles html

--- main.py
+++ main.py
@@ -60,3 +60,45 @@ async def kia_sportage() -> FileResponse:
     path = "static/images/kia-sportage.png"
     media_type = "image/png"
     return FileResponse(path=path, media_type=media_type)
+
+
+@app.get("/kia-picanto.png", tags=tags, response_class=FileResponse, status_code=status.HTTP_200_OK, include_in_schema=False)
+async def kia_picanto() -> FileResponse:
+    path = "static/images/kia-picanto.png"
+    media_type = "image/png"
+    return FileResponse(path=path, media_type=media_type)
+
+
+@app.get("/kia-seltos.png", tags=tags, response_class=FileResponse, status_code=status.HTTP_200_OK, include_in_schema=False)
+async def kia_seltos() -> FileResponse:
+    path = "static/images/kia-seltos.png"
+    media_type = "image/png"
+    return FileResponse(path=path, media_type=media_type)
+
+
+@app.get("/kia-carens.png", tags=tags, response_class=FileResponse, status_code=status.HTTP_200_OK, include_in_schema=False)
+async def kia_carens() -> FileResponse:
+    path = "static/images/kia-carens.png"
+    media_type = "image/png"
+    return FileResponse(path=path, media_type=media_type)
+
+
+@app.get("/kia-sorento.png", tags=tags, response_class=FileResponse, status_code=status.HTTP_200_OK, include_in_schema=False)
+async def kia_sorento() -> FileResponse:
+    path = "static/images/kia-sorento.png"
+    media_type = "image/png"
+    return FileResponse(path=path, media_type=media_type)
+
+
+@app.get("/kia-k2700.png", tags=tags, response_class=FileResponse, status_code=status.HTTP_200_OK, include_in_schema=False)
+async def kia_k2700() -> FileResponse:
+    path = "static/images/kia-k2700.png"
+    media_type = "image/png"
+    return FileResponse(path=path, media_type=media_type)
+
+
+@app.get("/kia-k3000.png", tags=tags, response_class=FileResponse, status_code=status.HTTP_200_OK, include_in_schema=False)
+async def kia_k3000() -> FileResponse:
+    path = "static/images/kia-k3000.png"
+    media_type = "image/png"
+    return FileResponse(path=path, media_type=media_type)
